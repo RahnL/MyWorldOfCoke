77766aa652c60ef13ff4bad5d285dbe0ce45ba3d
Rahn Lieberman <bigrahn@gmail.com>
2017-06-11 20:10:36 -0400

    Update to get colors working and fix the header.

--- MyWorldOfCokeMap/map.php
+++ MyWorldOfCokeMap/map.php
@@ -1,7 +1,7 @@
 ﻿<!DOCTYPE html>
 <meta charset="utf-8">
 <head>
-<title>My World of Coke Visualization</title>
+<title>My World of Coke Map of countries</title>
 <link rel="stylesheet" type="text/css" href="mapstyle.css" media="screen" />
 </head>
 
@@ -38,9 +38,9 @@ var data = [
 			 while($row = $result->fetch_assoc()) {
 				echo ("['c".$row["ISOM49Code"]);	// ID
 				echo ("','".$row["CountryName"]);	// CountryName
-				echo ("','".$row["isBottle"]);	// IsBottle
-				echo ("','".$row["DateAquired"]);
-				echo ("',], \n");
+				echo ("',".$row["isBottle"]);	// IsBottle
+				echo (",'".$row["DateAquired"]);
+				echo ("'], \n");
 			}
 		}
 		// Throw an extra line at the end to handle that trailing comma
@@ -49,93 +49,163 @@ var data = [
 		$conn->close();
 	?> ];
 </script>
-			
-<script>
+<div class="header">A partial map of where the collection is from</div>	
 
+ <script>
+            var width = 960,
+                height = 900;
 
-// Dynamically create this list of countries
+            var color = d3.scale.category10();
 
-var width = 960,
-    height = 580;
+            var projection = d3.geo.kavrayskiy7()
+                .scale(170)
+                .translate([width / 2, height / 3])
+                .precision(.1);
 
-var color = d3.scale.category10();
+            var path = d3.geo.path()
+                .projection(projection);
 
-var projection = d3.geo.kavrayskiy7()
-    .scale(170)
-    .translate([width / 2, height / 2])
-    .precision(.1);
+            var graticule = d3.geo.graticule();
 
-var path = d3.geo.path()
-    .projection(projection);
+            var svg = d3.select("div").append("svg")
+                .attr("width", width)
+                .attr("height", height);
 
-var graticule = d3.geo.graticule();
+            svg.append("defs").append("path")
+                .datum({ type: "Sphere" })
+                .attr("id", "sphere")
+                .attr("d", path);
 
-var svg = d3.select("div").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+            svg.append("use")
+                .attr("class", "stroke")
+                .attr("xlink:href", "#sphere");
 
-svg.append("defs").append("path")
-    .datum({type: "Sphere"})
-    .attr("id", "sphere")
-    .attr("d", path);
+            svg.append("use")
+                .attr("class", "fill")
+                .attr("xlink:href", "#sphere");
 
-svg.append("use")
-    .attr("class", "stroke")
-    .attr("xlink:href", "#sphere");
+            svg.append("path")
+                .datum(graticule)
+                .attr("class", "graticule")
+                .attr("d", path);
 
-svg.append("use")
-    .attr("class", "fill")
-    .attr("xlink:href", "#sphere");
+            d3.json("world-50m.json", function (error, world) {
+                if (error) throw error;
 
-svg.append("path")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("d", path);
+                var countries = topojson.feature(world, world.objects.countries).features,
+                    neighbors = topojson.neighbors(world.objects.countries.geometries);
 
-	
-d3.json("world-50m.json", function(error, world) {
-  if (error) throw error;
+                svg.selectAll(".country")
+                     .data(countries)
+                     .enter().insert("path", ".graticule")
+                     .attr("class", function (d) { return "c" + d.id; })
+                     .attr("d", path);
 
-  var countries = topojson.feature(world, world.objects.countries).features,
-      neighbors = topojson.neighbors(world.objects.countries.geometries);
+                svg.insert("path", ".graticule")
+                  .datum(topojson.mesh(world, world.objects.countries, function (a, b) { return a !== b; }))
+                  .attr("class", "boundary")
+                  .attr("d", path);
 
- svg.selectAll(".country")
-      .data(countries)
-	  .enter().insert("path", ".graticule")
-      .attr("class", function(d) { return "c" + d.id; })
-      .attr("d", path);
-	 
-	svg.insert("path", ".graticule")
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-      .attr("class", "boundary")
-      .attr("d", path);
-	
-  
-	 data.forEach(function (elt, i) {
+                //This loops through each element of our array from DB, and highlights it.               
+
+                data.forEach(function (elt, i) {
                     console.log(elt[0] + ': ' + elt[1]);
                     console.log(svg.select(elt[0]));
-					
-					var colr = 'blue';
-                     if (elt[2] == 'True') {
-                                colr = 'blue';
-                      }
+                    var colr = 'blue';
+                    if (elt[2] ==  1) {
+                        colr = 'red';       //Bottles are Red!
+                    }
 
                     svg.select('.' + elt[0]).attr('fill', colr)
-                        .append("svg:title")
+
                         .text(function (d) {
                             var c = elt[1];
                             var type = 'can';
-                            if (elt[2] == 'True') {
+                            if (elt[2] == 1) {
                                 type = 'bottle';
                             }
-                            var dt = elt[3];
-                            return c + "\nWe got a " + type + " from here on "+dt;
+                            var dt = elt[3];                           
+                            i.date = elt[3];
+
+                            return c + "\nWe got a " + type + " from here on " + dt;
                         });
-	});
-});
+                });
 
-d3.select(self.frameElement).style("height", height + "px");
+                var dtMin = "1/1/1985";
+                var dtMax = "12/31/2013";
+                
+                var format = d3.time.format("%d/%m/%Y");               
+                var startDate = format.parse(dtMin);
+                var endDate = format.parse(dtMax); 
 
-</script>
+                var x = d3.time.scale()
+                    .domain([startDate, endDate])
+                    .nice(d3.time.month)
+                    .range([100, width - 100]); 
 
-<div class="footer"><a href="about.html">About</a></div>
+                svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + (700) + ")")
+                //.attr("fill", 'grey')
+                .call(d3.svg.axis().scale(x).orient("bottom"));
+
+                data.forEach(function (elt, i) {
+                    var m1 = elt[3];                   
+                    var x1 = x(format.parse(m1));
+
+                    var colr = 'blue';
+                    if (elt[2] == 1) {
+                        colr = 'red';       //Bottles are Red!
+                    }
+
+                    svg.append("circle")
+                       .attr("r", 8)
+                       .attr("cx", x1)
+                       .attr("cy", 700)
+                       .attr("fill", colr);
+
+                    svg.append("text")                      
+                        //.attr("x", function (d) { return x1; })
+                        //.attr("y", 680)                      
+                        .style("fill", "black")
+                        .style("font-size", "12px")                        
+                        .attr("dy", ".35em")
+                        .attr("text-anchor", "start")
+                        .attr("transform", "translate(" + x1 + ",690) rotate(-60)")
+                        .text(elt[1]);
+                });
+
+                svg.append("circle")
+                        .attr("r", 8)
+                        .attr("cx", 10)
+                        .attr("cy", 10)
+                        .attr("fill", 'red');
+                        
+                svg.append("text")                                    
+                        .style("fill", "black")
+                        .style("font-size", "12px")
+                        .attr("x", 20)
+                        .attr("y", 15)
+                        .text("Coke Bottle");
+
+                svg.append("circle")
+                       .attr("r", 8)
+                       .attr("cx", 10)
+                       .attr("cy", 30)
+                       .attr("fill", 'blue');
+
+                svg.append("text")
+                       .style("fill", "black")
+                       .style("font-size", "12px")
+                       .attr("x", 20)
+                       .attr("y", 35)
+                       .text("Coke Can");
+
+
+
+            });
+            d3.select(self.frameElement).style("height", height + "px");       
+                        
+        </script>
+
+<div class="footer"><a href="about/">About</a></div>
